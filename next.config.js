6702364f2f024da615cc91075ae6ce8a5f766681
carlosduram71 <jorgemiranda0180@gmail.com>
2021-07-26 07:27:59 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports={
         domains:["djangoapi3.herokuapp.com","127.0.0.1",'links.papareact.com',"lh3.googleusercontent.com"]
     },
     env:{
-        stripe_public_key:process.env.STRIPE_PUBLIC_KEY
-    }
-}
+        stripe_public_key:process.env.STRIPE_PUBLIC_KEY,
+    },
+};
--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -17,7 +17,7 @@ export default async (req, res) =>{
     }));
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
-            shipping_address_collection:{
+        shipping_address_collection:{
             allowed_countries: ["GB","US","CA"],
         },
         line_items: transformedItems,
